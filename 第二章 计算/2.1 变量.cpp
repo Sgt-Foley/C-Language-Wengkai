1877451b5a21f55d626af753eab22e112f60516e
Sgt-Foley <112837804+Sgt-Foley@users.noreply.github.com>
2023-02-20 22:21:57 +0800

    class

---
+++ 第二章 计算/2.1 变量.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 0, j = 0;//变量定义，变量初始化赋值，int类型
+//	scanf("%d", &i);//变量输入，赋值给i
+//	j = i + 10;
+//	printf("i");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int AMOUNT = 100;//常量定义
+//	int price = 0;
+//
+//	printf("please print money:");
+//	scanf("%d", &price);
+//
+//	int change = AMOUNT - price;
+//	printf("give you %d yuan\n", change);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", 10 / 3 * 3);
+//	printf("%f", 10.0 / 3);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a;
+//	double b;
+//	scanf("%d %lf", a, b);
+//	printf("%d %f", a, b);
+//	return 0;
+//}
