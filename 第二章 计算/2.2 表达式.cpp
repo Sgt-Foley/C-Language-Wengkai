3dd00fe1d331034c5818e1bbaca136ac280af16a
Sgt-Foley <112837804+Sgt-Foley@users.noreply.github.com>
2023-02-20 22:25:04 +0800

    class

---
+++ 第二章 计算/2.2 表达式.cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()//计算时间差
+//{
+//	int hour1, minute1;
+//	int hour2, minute2;
+//	scanf("%d %d", &hour1, &minute1);
+//	scanf("%d %d", &hour2, &minute2);
+//
+//	int t1 = hour1 * 60 + minute1;
+//	int t2 = hour2 * 60 + minute2;
+//	int t = t1 - t2;
+//	printf("%d %d",t/60,t%60 );
+//
+//	return 0;
+//}
+
+//int main()//求平均值
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	double c = (a + b) / 2.0;
+//	printf("%lf", c);
+//
+//	return 0;
+//}
+
+//int main()//复利计算
+//{
+//	double a = 0;
+//	scanf("%lf\n", &a);
+//	double b = a * (1 + 0.033 ) * (1 + 0.033) * (1 + 0.033);
+//	printf("%lf", b);
+//
+//	return 0;
+//}
+
+//int main()//交换变量
+//{
+//	int a = 6, b = 5;
+//	int c = 0;
+//	printf("%d %d\n", a, b);
+//	c = a;//a=6,b=5,c=6
+//	a = b;//a=5,b=5,c=6
+//	b = c;//a=5,b=6,c=6
+//	printf("%d %d", a, b);
+//
+//	return 0;
+//}
+
+//int main()//复合赋值和递增递减
+//{
+//	int t = 0;
+//	t += 2;
+//	printf("%d\n", t);
+//	int a = 10;
+//	printf("%d\n", a++);
+//	printf("%d\n", a);
+//	a = 10;
+//	printf("%d\n", ++a);
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
